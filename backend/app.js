f376067c8ef11f6a675424bf84a1e38c6c62f782
Sofa Lis <sofyaocherednaya@yandex.ru>
2022-10-15 20:01:09 +0300

    Pr15 fix env

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: './.env' });
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const {
